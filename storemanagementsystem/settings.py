26f5d4cbf7dbcf069a3d7b922af1037e36f6d65a
NewUser <coleworldp@gmail.com>
2021-10-21 11:22:28 +0100

    'updated'

--- storemanagementsystem/settings.py
+++ storemanagementsystem/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t9(u(dt1c08jn@1+rny(c!iym7d*4fk*f#ada#ukn4jym)e_fo'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG_VALUE')
 
 ALLOWED_HOSTS = []
 
