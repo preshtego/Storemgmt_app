a4d6e9ec13d6eaefd2131c09887c6b7d895826c8
NewUser <coleworldp@gmail.com>
2021-10-23 20:08:46 +0100

    'updatedfiles'

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG_VALUE = os.environ.get('DEBUG_VALUE')
 
-print(STOREMGMT_SECRET_KEY)
-print(STOREMGMT_DEBUG_VALUE)
+print(SECRET_KEY)
+print(DEBUG_VALUE)
