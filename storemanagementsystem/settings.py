45730ddee714f69561f6d704320dc983df98578f
NewUser <coleworldp@gmail.com>
2021-10-24 00:21:00 +0100

    'update'

--- storemanagementsystem/settings.py
+++ storemanagementsystem/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '9cb2bf45cc6be18d9916103cc37411577efe488be312762b7b8b2e3b66'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
