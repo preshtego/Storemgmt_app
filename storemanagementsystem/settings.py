8697cd128de55f642c4977be43ffe8a3475783d0
NewUser <coleworldp@gmail.com>
2021-10-23 19:41:37 +0100

    'updatedfile'

--- storemanagementsystem/settings.py
+++ storemanagementsystem/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('STOREMGMT_SECRET_KEY')
+STOREMGMT_SECRET_KEY = os.environ.get('STOREMGMT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('STOREMGMT_DEBUG_VALUE')
+STOREMGMT_DEBUG = os.environ.get('STOREMGMT_DEBUG_VALUE')
 
 ALLOWED_HOSTS = ['storemgmt-app.herokuapp.com']
 
