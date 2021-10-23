5226af6aef1c8853d43e8b76ea37b6041819f82e
NewUser <coleworldp@gmail.com>
2021-10-23 21:32:23 +0100

    'update'

--- storemanagementsystem/settings.py
+++ storemanagementsystem/settings.py
@@ -151,5 +151,3 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
