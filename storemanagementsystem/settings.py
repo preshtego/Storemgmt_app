4bd604cfe073a9ea21b4f37723d8963f0b731c38
NewUser <coleworldp@gmail.com>
2021-10-23 20:39:04 +0100

    'fileupdate'

--- storemanagementsystem/settings.py
+++ storemanagementsystem/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 import os
 
 
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = True
 
 ALLOWED_HOSTS = ['storemgmt-app.herokuapp.com']
 
