0ea25247b67f9b1d55bfade035b1941877f508ff
NewUser <coleworldp@gmail.com>
2021-10-24 02:19:13 +0100

    'debug'

--- storemanagementsystem/settings.py
+++ storemanagementsystem/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['storemgmt-app.herokuapp.com']
 
 
 # Application definition
